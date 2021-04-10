4a188a12a5e6904ec3adbdfdd52e7ce036de215b
hisako.shimura <shimura.hisako@gmail.com>
2021-04-10 13:22:50 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'users/index'
   get 'users/new'
   get 'users/create'
@@ -14,6 +17,10 @@ Rails.application.routes.draw do
     root to: 'tasks#index'
     
     resources :tasks
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
     
     get 'signup', to: 'users#new'
     resources :users, only: [:index, :create]
