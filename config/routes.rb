20b278d02d26ab19cde5f61aa0f54c7b2c299933
hisako.shimura <shimura.hisako@gmail.com>
2021-04-13 10:43:47 +0000

    課題修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    # get 'tasks/:id', to: 'tasks#show'
-    # post 'tasks', to: 'tasks#create'
-    # put 'tasks/:id', to: 'tasks#update'
-    # delete 'tasks/:id', to: 'tasks#destroy'
+  root to: 'tasks#index'
 
-    # get 'tasks', to: 'tasks#index'
-    # get 'tasks/new', to: 'tasks#new'
-    # get 'tasks/:id/edit', to: 'tasks#edit'
-    root to: 'tasks#index'
-    
-    get 'login', to: 'sessions#new'
-    post 'login', to: 'sessions#create'
-    delete 'logout', to: 'sessions#destroy'
-    
-    get 'signup', to: 'users#new'
-    
-    resources :users, only: [:create]
-    resources :tasks
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  
+  resources :users, only: [:create]
+  resources :tasks
 end
